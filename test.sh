7301b5508170390f67e1712737384da49a0a1fb1
drifter089 <akshatmittal.0007@gmail.com>
2024-10-25 04:58:19 +0700

    fix dir-name test.sh

--- test.sh
+++ test.sh
@@ -10,12 +10,16 @@ pip install git+https://github.com/open-energy-transition/linopy.git@only-genera
 
 conda install -c conda-forge time
 
-cp -r benchmarks/pypsa/* pypsa-eur/config
+cp -r solver-benchmark/benchmarks/pypsa/* pypsa-eur/config
 
 mv pypsa-eur/config/solver_benchmark_pypsa_eur.py pypsa-eur/
 
 sed -i '/cf_solving = solving\["options"\]/a\ kwargs["keep_files"] = cf_solving.get("keep_files", True)' pypsa-eur/scripts/solve_network.py
 
 cd pypsa-eur
+
+ls
+
+# cat scripts/solve_network.py
 
 python solver_benchmark_pypsa_eur.py --configfile config/pypsa-eur-elec-10-lvopt-3h.yaml+
