d43f84f4dd9ad942a1ef195c38bee9c65695c639
drifter089 <akshatmittal.0007@gmail.com>
2024-10-25 17:06:33 +0700

    adding print at the end

--- test.sh
+++ test.sh
@@ -2,24 +2,34 @@
 
 ls
 
-# git clone https://github.com/PyPSA/pypsa-eur
-
-# git clone https://github.com/open-energy-transition/solver-benchmark
-
 pip install git+https://github.com/open-energy-transition/linopy.git@only-generate-problem-files --no-deps
 
 conda install -c conda-forge time
 
-cp -r solver-benchmark/benchmarks/pypsa/* pypsa-eur/config
+cp bench.py pypsa-eur/scripts/solve_network.py
 
-mv pypsa-eur/config/solver_benchmark_pypsa_eur.py pypsa-eur/
-
-sed -i '/cf_solving = solving\["options"\]/a\ kwargs["keep_files"] = cf_solving.get("keep_files", True)' pypsa-eur/scripts/solve_network.py
+cat pypsa-eur/scripts/solve_network.py 
 
 cd pypsa-eur
 
-ls
+BENCHMARK_DIR="../solver-benchmark/benchmarks/pypsa"
 
-# cat scripts/solve_network.py
+time python "$BENCHMARK_DIR/pypsa-gas+wind+sol+ely-1-1h.py"
 
-python solver_benchmark_pypsa_eur.py --configfile config/pypsa-eur-elec-10-lvopt-3h.yaml+
+time python "$BENCHMARK_DIR/pypsa-gas+wind+sol+ely-ucgas-1-1h.py"
+
+time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-trex-10-3h.yaml" ; echo -e '\a'    
+
+time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-sec-2-3h.yaml" ; echo -e '\a'
+
+time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-op-20-3h.yaml" ; echo -e '\a'
+
+time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-op-ucconv-20-3h.yaml" ; echo -e '\a'
+
+cd ..
+
+ls -R /tmp
+
+
+
+
