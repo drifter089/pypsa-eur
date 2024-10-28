9d4c3814ce4d6e362bcd92d33cdfb24ff8140125
drifter089 <akshatmittal.0007@gmail.com>
2024-10-28 17:37:01 +0700

    trial with tutorial file

--- test.sh
+++ test.sh
@@ -6,29 +6,35 @@ pip install git+https://github.com/open-energy-transition/linopy.git@only-genera
 
 conda install -c conda-forge time
 
+cp config_test.yaml pypsa-eur/config/test/config.electricity.yaml
 cp bench.py pypsa-eur/scripts/solve_network.py
 
 cat pypsa-eur/scripts/solve_network.py 
+cat pypsa-eur/config/test/config.electricity.yaml
 
 cd pypsa-eur
 
-BENCHMARK_DIR="../solver-benchmark/benchmarks/pypsa"
+snakemake -call all --cores all --printshellcmds --configfile  config/test/config.electricity.yaml 
 
-time python "$BENCHMARK_DIR/pypsa-gas+wind+sol+ely-1-1h.py"
+# BENCHMARK_DIR="../solver-benchmark/benchmarks/pypsa"
 
-time python "$BENCHMARK_DIR/pypsa-gas+wind+sol+ely-ucgas-1-1h.py"
+# time python "$BENCHMARK_DIR/pypsa-gas+wind+sol+ely-1-1h.py"
 
-time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-trex-10-3h.yaml" ; echo -e '\a'    
+# time python "$BENCHMARK_DIR/pypsa-gas+wind+sol+ely-ucgas-1-1h.py"
 
-time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-sec-2-3h.yaml" ; echo -e '\a'
+# time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-trex-10-3h.yaml" ; echo -e '\a'    
 
-time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-op-20-3h.yaml" ; echo -e '\a'
+# time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-sec-2-3h.yaml" ; echo -e '\a'
 
-time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-op-ucconv-20-3h.yaml" ; echo -e '\a'
+# time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-op-20-3h.yaml" ; echo -e '\a'
 
-cd ..
+# time snakemake -call all --cores all --printshellcmds --configfile "$BENCHMARK_DIR/pypsa-eur-elec-op-ucconv-20-3h.yaml" ; echo -e '\a'
 
-ls -R /tmp
+# time snakemake -call all --cores all --printshellcmds --configfile  config/test/config.electricity.yaml ; echo -e '\a'
+
+# cd ..
+
+# ls -R /tmp
 
 
 
